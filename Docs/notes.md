062a771eda1fbf9f620c951bd77a38f874e4c1f3
TunnyTraffic <liam.graves@gmail.com>
2020-09-12 23:27:17 +0200

    source & destination counts + start of payload parsing

--- Docs/notes.md
+++ Docs/notes.md
@@ -2,3 +2,4 @@ automayt pcaps from: https://github.com/automayt/ICS-pcap
 
 https://vnetman.github.io/pcap/python/pyshark/scapy/libpcap/2018/10/25/analyzing-packet-captures-with-python-part-1.html
 
+https://scapy.readthedocs.io/en/latest/api/scapy.contrib.modbus.html
--- SCADACap.py
+++ SCADACap.py
@@ -1,11 +1,20 @@
-# starting with
-
 import argparse
 import os
 import sys
 from scapy.utils import RawPcapReader
 from scapy.layers.l2 import Ether
 from scapy.layers.inet import IP, TCP
+import scapy.contrib.modbus as scmb
+from collections import Counter
+
+#class ModBusPDU:
+#    def __init__(self, rawdata):
+#        self.rawdata = rawdata
+
+
+
+
+
 
 
 def process_pcap(file_name):
@@ -13,6 +22,8 @@ def process_pcap(file_name):
 
     count = 0
     interesting_packet_count = 0
+    sources = []
+    destinations = []
 
     for (pkt_data, pkt_metadata,) in RawPcapReader(file_name):
         count += 1
@@ -32,10 +43,38 @@ def process_pcap(file_name):
             # Ignore non-TCP packet
             continue
 
+        # print(ip_pkt.summary())
+
+
+        source = ip_pkt.fields['src']
+        destination = ip_pkt.fields['dst']
+        payload = ip_pkt[TCP].payload
+        sources.append(source)
+        destinations.append(destination)
+
+        print('Source: ', source)
+        print('Destination: ', destination)
+
+        print('Raw payload: ',bytes(payload))
+
+        modbuspacket = scmb.ModbusAD
+        #    function_code = scmb.orb(ip_pkt[TCP].payload[0])
+        print(function_code)
+
+        #print(ip_pkt[0].show()) #this one shows everything for debugging
+
         interesting_packet_count += 1
 
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+    s = Counter(sources)
+    d = Counter(destinations)
+    print('Source counts: ',s)
+    print('Destination counts: ',d)
     print('{} contains {} packets ({} interesting)'.
           format(file_name, count, interesting_packet_count))
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PCAP reader')
